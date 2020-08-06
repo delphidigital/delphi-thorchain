acfad6e924b363303b73a8e551f008b8358d7c21
Luke Saunders <luke.saunders@gmail.com>
2020-08-06 18:58:31 +0100

    get better GA tracking id

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     ['@nuxtjs/google-analytics', {
-      id: 'G-SC8BNHWS34',
+      id: 'UA-174637134-1',
     }],
   ],
   env: {
