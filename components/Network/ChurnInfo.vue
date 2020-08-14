8c72ea5275bd9ac47b172bf33321f617f0b814e7
Federico Rodriguez <federodriguez.88@gmail.com>
2020-08-14 07:50:04 -0300

    Make countdown tick based on a time in milliseconds

--- components/Network/ChurnInfo.vue
+++ components/Network/ChurnInfo.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable -->
 <template>
   <div class="section">
     <div class="section__header">
@@ -9,19 +10,19 @@
       <div class="countdown__gauge">
         <svg height="125" width="240">
           <line
-            v-for="n in 40"
+            v-for="n in breakPoints"
             :key="n"
-            :x1="gaugeWidth / 2 + minRadius * Math.cos(breakPointAngle(n, 40))"
-            :x2="gaugeWidth / 2 + breakPointMaxRadius(n) * Math.cos(breakPointAngle(n, 40))"
-            :y1="gaugeHeight - minRadius * Math.sin(breakPointAngle(n, 40))"
-            :y2="gaugeHeight - breakPointMaxRadius(n) * Math.sin(breakPointAngle(n, 40))"
+            :x1="gaugeWidth / 2 + minRadius * Math.cos(breakPointAngle(n, breakPoints))"
+            :x2="gaugeWidth / 2 + breakPointMaxRadius(n) * Math.cos(breakPointAngle(n, breakPoints))"
+            :y1="(gaugeHeight - 2) - minRadius * Math.sin(breakPointAngle(n, breakPoints))"
+            :y2="(gaugeHeight - 2) - breakPointMaxRadius(n) * Math.sin(breakPointAngle(n, breakPoints))"
             :stroke="breakPointColor(n)"
             :stroke-width="n === currentBreakpoint ? 3 : 1"
           />
         </svg>
         <p class="countdown__gauge__text">
           <span class="countdown__gauge__time">
-            10m : 20s
+            {{ formatTime(timeRemaining) }}
           </span>
           <span class="text__label">
             time remaining
@@ -46,7 +47,15 @@
 </template>
 
 <script>
+const seconds = 1000;
+const minutes = 60 * seconds;
+
 export default {
+  data() {
+    return {
+      timeRemaining: 1 * minutes,
+    };
+  },
   computed: {
     minRadius() {
       return 105;
@@ -60,20 +69,46 @@ export default {
     gaugeHeight() {
       return 125;
     },
+    maxTime() {
+      return 15 * minutes;
+    },
+    breakPoints() {
+      return 40;
+    },
     currentBreakpoint() {
       // TODO(Fede): Do this dynamically based on time remaining
-      return 20;
+      return (
+        this.breakPoints - Math.ceil((this.timeRemaining / this.maxTime) * this.breakPoints)
+      ) + 1;
     },
+  },
+  mounted() {
+    this.tick();
   },
   methods: {
     breakPointAngle(bp, max) {
-      return (Math.PI * (bp - 1)) / (max - 1);
+      const angle = (Math.PI * (bp - 1)) / (max - 1);
+      return angle;
     },
     breakPointMaxRadius(bp) {
       return bp === this.currentBreakpoint ? (this.maxRadius + 3) : this.maxRadius;
     },
     breakPointColor(bp) {
       return bp < this.currentBreakpoint ? '#fff' : '#16CEB9';
+    },
+    formatTime(milliseconds) {
+      const minsRemaining = Math.floor(milliseconds / minutes);
+      const secondsRemaining = milliseconds % minutes;
+
+      return `${minsRemaining}m : ${secondsRemaining / 1000}s`;
+    },
+    tick() {
+      setInterval(() => {
+        this.timeRemaining -= 1 * seconds;
+        if (this.timeRemaining < 0) {
+          this.timeRemaining = this.maxTime;
+        }
+      }, 1 * seconds);
     },
   },
 };
@@ -102,7 +137,7 @@ export default {
   flex-direction: column;
   margin: 0 auto;
   text-align: center;
-  bottom: 30px;
+  bottom: 32px;
 }
 
 .countdown__gauge__time {
