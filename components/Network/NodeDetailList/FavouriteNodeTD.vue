35830de217d2927fe44e252d76cb1eab438153b4
Federico Rodriguez <federodriguez.88@gmail.com>
2020-08-15 22:01:18 -0300

    Add favourite node toggle

--- components/Network/NodeDetailList/FavouriteNodeTD.vue
+++ components/Network/NodeDetailList/FavouriteNodeTD.vue
@@ -1,9 +1,12 @@
 <template>
-  <td :class="`section__table__data section__table__data--starred ${tdClass()}`">
-    <span class="toggle" @click="toggle">
+  <td class="favourite-node section__table__data" :class="{'favourite-node--on': starred}">
+    <span class="favourite-node-toggle" @click="toggle">
       <img v-if="!starred" src="/flash-off.svg"></img>
       <img v-if="starred" src="/flash-on.svg"></img>
     </span>
+    <div class="app-tooltip favourite-node-tooltip">
+      Highlight this node so you can find it more easily using this browser.
+    </div>
   </td>
 </template>
 
@@ -14,13 +17,6 @@ export default {
       type: String,
       default: '',
     },
-  },
-  data() {
-    return {
-      tdClass() {
-        return this.starred ? 'section__table__data--starred--on' : '';
-      },
-    };
   },
   computed: {
     starred() {
@@ -36,14 +32,44 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .toggle {
+  .favourite-node-toggle {
     cursor: pointer;
   }
-  .section__table__data--starred {
+  .favourite-node {
     border-left: 2px solid transparent;
     transition-duration: 1s;
+    position: relative;
+
+    &:hover {
+      .favourite-node-tooltip {
+        display: block;
+      }
+    }
   }
-  .section__table__data--starred--on {
+  .favourite-node--on {
     border-left: 2px solid $color-yellow;
+  }
+  .favourite-node-tooltip {
+    position: absolute;
+    right: -255px;
+    top: -5px;
+    background-color: $color-bg-popup;
+    font-size: 12px;
+    font-weight: 400;
+    border-radius: 4px;
+    width: 250px;
+    padding: 15px;
+    display: none;
+  }
+  .favourite-node-tooltip:before {
+    content: "";
+    position: absolute;
+    width: 0px;
+    height: 0px;
+    border-top: 6px solid transparent;
+    border-bottom: 6px solid transparent;
+    border-right: 6px solid $color-bg-popup;
+    right: calc(100%);
+    top: calc(50% - 6px);
   }
 </style>
