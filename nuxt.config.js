4293fdff7e0cd0a322fb99427c5b3b5be71d5dbd
Rodrigo Pimentel <sr.rodrigopv@gmail.com>
2021-02-22 09:28:06 -0500

    nuxt fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 import webpack from 'webpack';
 import ESLintPlugin from 'eslint-webpack-plugin';
 
+const APP_URL = process.env.NODE_ENV === 'production' && process.env.HOST && process.env.PORT
+  ? `http://${process.env.HOST}:${process.env.PORT}`
+  : process.env.APP_URL || 'http://localhost:3021',
+
 export default {
   /*
   ** Nuxt target
@@ -72,8 +76,11 @@ export default {
     pollingFrequency: process.env.POLLING_FREQUENCY || 5000,
     TESTNET_NODE_IP: process.env.TESTNET_NODE_IP,
     CHAOSNET_NODE_IP: process.env.CHAOSNET_NODE_IP,
-    APP_URL: process.env.APP_URL || 'http://localhost:3021',
+    APP_URL,
   },
+
+  http://localhost:3021
+  
   /*
   ** Nuxt.js modules
   */
