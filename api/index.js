d5a0c8140b8f035f3c12cf103c08c66d35b7607e
Ubuntu <user@ubuntu>
2021-04-12 13:01:48 -0500

    cors fix

--- api/index.js
+++ api/index.js
@@ -10,6 +10,14 @@ const thorchain = express.Router();
 const chaosnet = express.Router();
 const testnet = express.Router();
 
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 async function loadCached(id) {
   const rawdata = await getAsync(`thorchain::${id}`);
   return JSON.parse(rawdata);
